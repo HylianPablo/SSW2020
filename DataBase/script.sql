bfb9c04fc0339766bdda246e7f393f22b2fe09f5
Pablo Martínez <pablomartinez1017@gmail.com>
2020-04-07 13:21:25 +0200

    Arreglado TomCat, MySQL y scriptSQL

--- DataBase/script.sql
+++ DataBase/script.sql
@@ -1,20 +1,27 @@
-drop table if exists Usuario;
-drop table if exists Guardado;
-drop table if exists Dieta;
-drop table if exists Entrada;
-drop table if exists Comentario;
-drop table if exists PlatoMenu;
-drop table if exists Plato;
 drop table if exists PertenenciaPlato;
 drop table if exists Ingrediente;
- 
+drop table if exists PlatoMenu;
+drop table if exists Plato;
+drop table if exists Comentario;
+drop table if exists Entrada;
+drop table if exists Guardado;
+drop table if exists Usuario;
+drop table if exists Dieta;
+
+create table Dieta(
+	codigoDieta	char(20) not null,
+	titulo		char(20) not null,
+	descripcion	text not null,
+	primary key(codigoDieta)
+);
+
 create table Usuario(
 	nombre					char(50) not null,
 	nombreUsuario				char(20) not null,
 	contrasena				char(20) not null,
 	correo					char(40) not null,
 	favorito				char(20),
-	foreign key(favorito) references Dieta,
+	foreign key(favorito) references Dieta(codigoDieta),
 	primary key(nombreUsuario)
 );
 
@@ -22,55 +29,36 @@ create table Usuario(
 create table Guardado(
 	nombreUsuario				char(20) not null,
 	codigoDieta				char(20) not null,
-	foreign key(nombreUsuario) references Usuario,
-	foreign key(codigoDieta) references Dieta,
+	foreign key(nombreUsuario) references Usuario(nombreUsuario),
+	foreign key(codigoDieta) references Dieta(codigoDieta),
 	primary key(nombreUsuario, codigoDieta)
 );
 
 create table Entrada(
 	codigoEntrada				char(20) not null,
 	titulo					char(50) not null,
-	cuerpo					char(500),
+	cuerpo					text,
 	nombreUsuario				char(20) not null,
 	fecha					date not null,
-	foreign key(nombreUsuario) references Usuario,
+	foreign key(nombreUsuario) references Usuario(nombreUsuario),
 	primary key(codigoEntrada)
 );
 
 create table Comentario(
 	codigoComentario			char(20) not null,
 	codigoPadre				char(20) not null,
-	cuerpo					char(500) not null,
+	cuerpo					text not null,
 	nombreUsuario				char(20) not null,
 	fecha					date not null,
-	foreign key(codigoPadre) references Entrada,
-	foreign key(nombreUsuario) references Usuario,
+	foreign key(codigoPadre) references Entrada(codigoEntrada),
+	foreign key(nombreUsuario) references Usuario(nombreUsuario),
 	primary key(codigoComentario)
-);
-
-create table PlatoMenu(
-	codigoPlato				char(20) not null,
-	codigoDieta				char(20) not null,
-	diaSemana				char(20) not null,
-	check (diaSemana in ('lunes','martes','miercoles','jueves','viernes','sabado','domingo')),
-	momento					char(30) not null,
-	check (momento in ('desayuno','comidaPrimero','comidaSegundo','cena')),
-	foreign key(codigoPlato) references Plato, 
-	foreign key(codigoDieta) references Dieta,
-	primary key(codigoPlato, codigoDieta)
 );	
-
-create table Dieta(
-	codigoDieta	char(20) not null,
-	titulo		char(20) not null,
-	descripcion	char(500) not null,
-	primary key(codigoDieta)
-);
 
 create table Plato(
 	codigoPlato 				char(20) not null,
 	nombre					char(30) not null,
-	descripcion				char(500),
+	descripcion				text,
 	desayuno				boolean not null,
 	vegano					boolean not null,
 	vegetariano				boolean not null,
@@ -85,12 +73,16 @@ create table Plato(
 	primary key(codigoPlato)
 );
 
-create table PertenenciaPlato(
-	codigoIngrediente		char(20) not null,
-	codigoPlato 			char(20) not null,
-	foreign key(codigoIngrediente) references Ingrediente,
-	foreign key(codigoPlato) references Plato,
-	primary key(codigoIngrediente, codigoPlato)
+create table PlatoMenu(
+	codigoPlato				char(20) not null,
+	codigoDieta				char(20) not null,
+	diaSemana				char(20) not null,
+	check (diaSemana in ('lunes','martes','miercoles','jueves','viernes','sabado','domingo')),
+	momento					char(30) not null,
+	check (momento in ('desayuno','comidaPrimero','comidaSegundo','cena')),
+	foreign key(codigoPlato) references Plato(codigoPlato), 
+	foreign key(codigoDieta) references Dieta(codigoDieta),
+	primary key(codigoPlato, codigoDieta)
 );
 
 create table Ingrediente(
@@ -103,13 +95,25 @@ create table Ingrediente(
 	primary key(codigoIngrediente)
 );
 
-insert into Usuario Values ('Pedro Sanz Lopez', 'pedsanz', '12345pedro','pedrosanzl@gmail.com','36587J');
-insert into Usuario Values ('Marta Sanchez Rojo', 'martsanro', 'farola4','marta1912@gmail.com','93163K');
-insert into Usuario Values ('Marcos Moya Gallo', 'marmoyaga', 'tirabuzon89','marcos.mg@yahoo.com','73803A');
+create table PertenenciaPlato(
+	codigoIngrediente		char(20) not null,
+	codigoPlato 			char(20) not null,
+	foreign key(codigoIngrediente) references Ingrediente(codigoIngrediente),
+	foreign key(codigoPlato) references Plato(codigoPlato),
+	primary key(codigoIngrediente, codigoPlato)
+);
 
-insert into Guardado Values ('pedsanz','36587J');
-insert into Guardado Values ('martsanro','93163K');
-insert into Guardado Values ('marmoyaga','73803A');
+insert into Dieta Values ('ASDF9999','Dieta vegeteriana','Una dieta que no lleva animales, salvo ovolácteos');
+insert into Dieta Values ('RJUK1312','Dieta mediterranea','Una dieta basada en la comida tradicional de la costa mediterranea');
+insert into Dieta Values ('HIWH1370','Dieta de deportistas','Una dieta orientada a personas que realizan mucho deporte a diario');
+
+insert into Usuario Values ('Pedro Sanz Lopez', 'pedsanz', '12345pedro','pedrosanzl@gmail.com','ASDF9999');
+insert into Usuario Values ('Marta Sanchez Rojo', 'martsanro', 'farola4','marta1912@gmail.com','ASDF9999');
+insert into Usuario Values ('Marcos Moya Gallo', 'marmoyaga', 'tirabuzon89','marcos.mg@yahoo.com','ASDF9999');
+
+insert into Guardado Values ('pedsanz','ASDF9999');
+insert into Guardado Values ('martsanro','ASDF9999');
+insert into Guardado Values ('marmoyaga','ASDF9999');
 
 insert into Entrada Values ('AKA78','Quiero comer bien','Quiero tener una alimentacion sana y no se cocinar','pedsanz','2020-04-03');
 insert into Entrada Values ('JTG45','Quiero ser vegana',NULL,'martsanro','2020-03-23');
@@ -119,22 +123,18 @@ insert into Comentario Values ('65JH9','AKA78','Prueba a no comer bollos','marmo
 insert into Comentario Values ('90TR1','AKA78','El aguacate es un alimento muy sano, incluyelo en tus platos','marmoyaga','2020-04-06');
 insert into Comentario Values ('39DV4','JTG45','No comas carne','pedsanz','2020-03-25');
 
-insert into PlatoMenu Values ('6480FAHJ','RJUK1312','lunes','comidaPrimero');
-insert into PlatoMenu Values ('2019WEMO','RJUK1312','lunes','comidaSegundo');
-insert into PlatoMenu Values ('3109YRYU','ASDF9999','jueves','cena');
-
-insert into Dieta Values ('ASDF9999','Dieta vegeteriana','Una dieta que no lleva animales, salvo ovolácteos');
-insert into Dieta Values ('RJUK1312','Dieta mediterranea','Una dieta basada en la comida tradicional de la costa mediterranea');
-insert into Dieta Values ('HIWH1370','Dieta de deportistas','Una dieta orientada a personas que realizan mucho deporte a diario');
-
 insert into Plato Values ('3109YRYU','Garbanzos con espinacas','Garbanzos de la huerta cocinados con espinaca baby y aceite de oliva',FALSE,TRUE,TRUE,FALSE,FALSE,350,10,10,10,50,20);
 insert into Plato Values ('2019WEMO','Pez espada','Pez espada con limón y romero, marinado con piñones',FALSE,FALSE,FALSE,TRUE,FALSE,280,10,20,30,20,20);
 insert into Plato Values ('6480FAHJ','Macarrones a la bolonesa','Macarrones con carne picada, tomate y orégano',FALSE,FALSE,FALSE,FALSE,TRUE,400,10,10,10,10,60);
 
-insert into PertenenciaPlato Values('98HJHE98','3109YRYU');
-insert into PertenenciaPlato Values('56WKEP56','3109YRYU');
-insert into PertenenciaPlato Values('21DHIU21','2019WEMO');
+insert into PlatoMenu Values ('6480FAHJ','RJUK1312','lunes','comidaPrimero');
+insert into PlatoMenu Values ('2019WEMO','RJUK1312','lunes','comidaSegundo');
+insert into PlatoMenu Values ('3109YRYU','ASDF9999','jueves','cena');
 
 insert into Ingrediente Values('98HJHE98','Garbanzo',TRUE,TRUE,FALSE,FALSE);
 insert into Ingrediente Values('56WKEP56','Espinaca',TRUE,TRUE,FALSE,FALSE);
 insert into Ingrediente Values('21DHIU21','Pez espada',FALSE,FALSE,FALSE,FALSE);
+
+insert into PertenenciaPlato Values('98HJHE98','3109YRYU');
+insert into PertenenciaPlato Values('56WKEP56','3109YRYU');
+insert into PertenenciaPlato Values('21DHIU21','2019WEMO');
---
+++ NetBeansProject/SSW/web/DataBase/script.sql
@@ -0,0 +1,1 @@
+INSERT a INTO a;
